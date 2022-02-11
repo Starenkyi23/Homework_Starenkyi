0aed15ba81b605749c2940c1eb96b06a2f5d2170
starenkyi23 <gonduras23>
2022-02-11 23:51:47 +0200

    hm

---
+++ hm2.py
@@ -0,0 +1,23 @@
+#task 1
+name = "Nick"
+day = "Friday"
+s = "Good day {}! {} is a perfect day to learn some python."
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+print(s.format(name, day))
+print("Good day {1}! {0} is a perfect day to learn some python.".format(day,name))
+
+#task 2
+first_name ="Nick"
+second_name = " Starenkyi"
+print(first_name + second_name)
+
+#task 3
+a = 8
+b = 3
+print(a+b)
+print(a-b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(abs(b-a))
+print(a // b)
