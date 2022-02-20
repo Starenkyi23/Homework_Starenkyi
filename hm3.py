d70288ebb40686d01fb2fa0d3b9d0a4905a6a063
starenkyi23 <gonduras23>
2022-02-20 22:09:50 +0200

    Task 3 Change

--- hm3.py
+++ hm3.py
@@ -1,20 +1,21 @@
 #Task 1
-y=str(input("str: "))
-if len(y) >= 2:
-    print(y[0:2],y[-2:len(y)],sep="")
-else: len(y) < 2,
-print(" ")
+# y=str(input("str: "))
+# if len(y) >= 2:
+#     print(y[0:2],y[-2:len(y)],sep="")
+# else: len(y) < 2,
+# print(" ")
 
 #Task 2
-x=int(input("phone number: "))
-if len(str(x)) < 10:
-    print("please, write correct number")
-elif len(str(x)) == 10:
-    print("OK")
+# x=int(input("phone number: "))
+# if len(str(x)) < 10:
+#     print("please, write correct number")
+# elif len(str(x)) == 10:
+#     print("OK")
 
 #Task 3
 z='nick'
 t=input('Введите имя: ')
-for i in t:
-    if i in z:
-        print(t.lower())
+if t.lower() == z:
+    print(True)
+else:
+    print(False)
