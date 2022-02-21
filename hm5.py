c6f41b43dcdcd6b2e133bfb86e11cdb395d5b539
starenkyi23 <gonduras23>
2022-02-21 20:26:38 +0200

    hm

--- hm5.py
+++ hm5.py
@@ -1,19 +1,19 @@
 # Списки, кортежі та множини
 #Task 1
-# import random
-# i= random.sample(range(0, 1000), 10)
-# print(i,'\n',max(i))
+import random
+i= random.sample(range(0, 1000), 10)
+print(i,'\n',max(i))
 
 #Task 2 (Я не придумал как сделать с помощью While)
-# import random
-# a= random.sample(range(0, 20), 10)
-# b= random.sample(range(0, 20), 10)
-# z=[]
-# for i in a:
-#     for j in b:
-#         if i == j:
-#             z.append(i)
-# print(a,'\n',b,'\n',z)
+import random
+a= random.sample(range(0, 20), 10)
+b= random.sample(range(0, 20), 10)
+z=[]
+for i in a:
+    for j in b:
+        if i == j:
+            z.append(i)
+print(a,'\n',b,'\n',z)
 
 #Task 3
 lst= list(range(0, 101))
